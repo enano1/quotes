1bc47a353ccea96f31cfc800751090dc51db62fa
Paul Martin Enano <paul.m.enano@gmail.com>
2024-09-22 17:48:55 -0400

    added some changes

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/bin/bash
+
+if command -v python3 &>/dev/null; then
+    echo "Python3 found, proceeding with pip installation..."
+    python3 -m pip install -r requirements.txt
+else
+    echo "Python3 not found. Aborting."
+    exit 1
+fi
+
+# Collect static files after pip installation
+python3 manage.py collectstatic --noinput
